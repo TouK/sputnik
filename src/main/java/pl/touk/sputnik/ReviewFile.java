efa59c857ce2a713afa17a4f588c9cb09862a6aa
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2014-03-17 11:33:10 +0100

    ReviewFile knows its source and target dirs.

--- src/main/java/pl/touk/sputnik/ReviewFile.java
+++ src/main/java/pl/touk/sputnik/ReviewFile.java
@@ -1,5 +1,6 @@
 package pl.touk.sputnik;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -7,6 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewFile {
+    private static final String MAVEN_ENTRY_REGEX = ".*src/(main|test)/java/";
+    private static final String JAVA = "java/";
+    private static final String SRC_MAIN = "src/main/";
+    private static final String BUILD_MAIN = "target/classes/";
+    private static final String SRC_TEST = "src/test/";
+    private static final String BUILD_TEST = "target/test-classes/";
+    private static final String DOT = ".";
+
     private final String gerritFilename;
     private final File ioFile;
     private final List<Comment> comments = new ArrayList<Comment>();
@@ -29,5 +38,26 @@ public class ReviewFile {
     @NotNull
     public List<Comment> getComments() {
         return comments;
+    }
+
+    @NotNull
+    public String getJavaClassName() {
+        return StringUtils.substringBeforeLast(gerritFilename.replaceFirst(MAVEN_ENTRY_REGEX, ""), DOT).replace('/', '.');
+    }
+
+    @NotNull
+    public String getSourceDir() {
+        return StringUtils.substringBeforeLast(gerritFilename, JAVA).concat(JAVA);
+    }
+
+    @NotNull
+    public String getBuildDir() {
+        if (StringUtils.contains(gerritFilename, SRC_MAIN)) {
+            return StringUtils.substringBeforeLast(gerritFilename, SRC_MAIN).concat(BUILD_MAIN);
+        }
+        if (StringUtils.contains(gerritFilename, SRC_TEST)) {
+            return StringUtils.substringBeforeLast(gerritFilename, SRC_TEST).concat(BUILD_TEST);
+        }
+        return getSourceDir();
     }
 }
---
+++ src/test/java/pl/touk/sputnik/ReviewFileTest.java
@@ -0,0 +1,71 @@
+package pl.touk.sputnik;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReviewFileTest {
+    private static final String MAIN_FILENAME = "gerrit-server/src/main/java/com/google/gerrit/server/project/RefControl.java";
+    private static final String MAIN_JAVA_CLASS_NAME = "com.google.gerrit.server.project.RefControl";
+    private static final String MAIN_JAVA_SOURCE_DIR = "gerrit-server/src/main/java/";
+    private static final String MAIN_JAVA_BUILD_DIR = "gerrit-server/target/classes/";
+
+    private static final String TEST_FILENAME = "gerrit-server/src/test/java/com/google/gerrit/server/project/RefControlTest.java";
+    private static final String TEST_JAVA_CLASS_NAME = "com.google.gerrit.server.project.RefControlTest";
+    private static final String TEST_JAVA_SOURCE_DIR = "gerrit-server/src/test/java/";
+    private static final String TEST_JAVA_BUILD_DIR = "gerrit-server/target/test-classes/";
+
+    @Test
+    public void shouldReturnMainJavaClassName() {
+        //given
+        ReviewFile reviewFile = new ReviewFile(MAIN_FILENAME);
+
+        //expect
+        assertThat(reviewFile.getJavaClassName()).isEqualTo(MAIN_JAVA_CLASS_NAME);
+    }
+
+    @Test
+    public void shouldReturnMainJavaSourceDirectory() {
+        //given
+        ReviewFile reviewFile = new ReviewFile(MAIN_FILENAME);
+
+        //expect
+        assertThat(reviewFile.getSourceDir()).isEqualTo(MAIN_JAVA_SOURCE_DIR);
+    }
+
+    @Test
+    public void shouldReturnMainJavaBuildDirectory() {
+        //given
+        ReviewFile reviewFile = new ReviewFile(MAIN_FILENAME);
+
+        //expect
+        assertThat(reviewFile.getBuildDir()).isEqualTo(MAIN_JAVA_BUILD_DIR);
+    }
+
+    @Test
+    public void shouldReturnTestJavaClassName() {
+        //given
+        ReviewFile reviewFile = new ReviewFile(TEST_FILENAME);
+
+        //expect
+        assertThat(reviewFile.getJavaClassName()).isEqualTo(TEST_JAVA_CLASS_NAME);
+    }
+
+    @Test
+    public void shouldReturnTestJavaSourceDirectory() {
+        //given
+        ReviewFile reviewFile = new ReviewFile(TEST_FILENAME);
+
+        //expect
+        assertThat(reviewFile.getSourceDir()).isEqualTo(TEST_JAVA_SOURCE_DIR);
+    }
+
+    @Test
+    public void shouldReturnTestJavaBuildDirectory() {
+        //given
+        ReviewFile reviewFile = new ReviewFile(TEST_FILENAME);
+
+        //expect
+        assertThat(reviewFile.getBuildDir()).isEqualTo(TEST_JAVA_BUILD_DIR);
+    }
+}
