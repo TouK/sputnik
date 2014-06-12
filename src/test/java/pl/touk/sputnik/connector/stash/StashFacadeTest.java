75cfebc6a496dfe636287f96c6917a6c9a18e929
Piotr Jagielski <pjg@touk.pl>
2014-06-13 00:08:19 +0200

    another merge

--- src/test/java/pl/touk/sputnik/connector/stash/StashFacadeTest.java
+++ src/test/java/pl/touk/sputnik/connector/stash/StashFacadeTest.java
@@ -9,52 +9,59 @@ import org.junit.Test;
 import pl.touk.sputnik.configuration.ConfigurationSetup;
 import pl.touk.sputnik.review.ReviewFile;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StashFacadeTest {
 
     private static Integer PORT = 8089;
-    private static final ImmutableMap<String, String> STASH_CONFIG_MAP = ImmutableMap.of(
+    private static String SOME_PULL_REQUEST_ID = "12314";
+    private static String SOME_REPOSITORY = "repo";
+    private static String SOME_PROJECT_KEY = "key";
+
+    private static final Map<String, String> STASH_CONFIG_MAP = ImmutableMap.of(
             "stash.host", "localhost",
             "stash.port", PORT.toString(),
             "stash.username", "user",
             "stash.password", "pass",
             "stash.useHttps", "false"
     );
-    private static final ImmutableMap<String, String> STASH_PATCHSET_MAP = ImmutableMap.of(
-            "cli.pullRequestId", "12",
-            "stash.repositorySlug", "myproject",
-            "stash.projectKey", "mykey"
+    private static final Map<String, String> STASH_PATCHSET_MAP = ImmutableMap.of(
+            "cli.pullRequestId", SOME_PULL_REQUEST_ID,
+            "stash.repositorySlug", SOME_REPOSITORY,
+            "stash.projectKey", SOME_PROJECT_KEY
     );
 
-    private StashFacade stashFacade;
+    private StashFacade fixture;
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(PORT);
 
     @Before
     public void setUp() {
-        Map<String, String> joinedMap = new HashMap<>();
-        joinedMap.putAll(STASH_CONFIG_MAP);
-        joinedMap.putAll(STASH_PATCHSET_MAP);
-        new ConfigurationSetup().setUp(joinedMap);
-        stashFacade = new StashFacadeBuilder().build();
+        new ConfigurationSetup().setUp(STASH_CONFIG_MAP, STASH_PATCHSET_MAP);
+        fixture = new StashFacadeBuilder().build();
     }
 
     @Test
     public void shouldGetChangeInfo() throws Exception {
-        stubFor(get(urlEqualTo("/rest/api/1.0/projects/mykey/repos/myproject/pull-requests/12/changes"))
+        String url = String.format(
+                "/rest/api/1.0/projects/%s/repos/%s/pull-requests/%s/changes",
+                SOME_PROJECT_KEY, SOME_REPOSITORY, SOME_PULL_REQUEST_ID
+        );
+        stubFor(get(urlEqualTo(url))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
                         .withBody(IOUtils.toString(getClass().getResourceAsStream("/stash-changes.json")))));
 
-        List<ReviewFile> files = stashFacade.listFiles();
+        List<ReviewFile> files = fixture.listFiles();
         assertThat(files).hasSize(4);
     }
 }
