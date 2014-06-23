750989e79280dd624dfba9cab654796199b3be82
Karol Lassak <ingwar@ingwar.eu.org>
2014-06-23 13:11:56 +0200

    Added tests for processing test files option
    - Extra tests added

--- src/test/java/pl/touk/sputnik/review/ReviewTest.java
+++ src/test/java/pl/touk/sputnik/review/ReviewTest.java
@@ -6,23 +6,51 @@ import pl.touk.sputnik.connector.gerrit.json.ReviewFileComment;
 import pl.touk.sputnik.connector.gerrit.json.ReviewInput;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Before;
 
 public class ReviewTest {
+    
+    List<ReviewFile> reviewList;
+    
+    @Before
+    public void prepare() {
+        reviewList = ImmutableList.of(
+                new ReviewFile("/src/main/java/file1.java"),
+                new ReviewFile("/src/main/java/file2.java"),
+                new ReviewFile("/src/test/java/file1.java"),
+                new ReviewFile("/src/test/java/file2.java")
+                );
+    }
 
     @Test
     public void shouldConvertToReviewInput() {
         //given
-        Review review = new Review(ImmutableList.of(new ReviewFile("test")), true);
+        Review review = new Review(reviewList, true);
 
         //when
         ReviewInput reviewInput = review.toReviewInput();
 
         //then
         assertThat(reviewInput.comments)
-                .hasSize(1)
-                .containsEntry("test", Collections.<ReviewFileComment>emptyList());
+                .hasSize(4);
+    }
+    
+    @Test
+    public void shouldNotProcessTestFiles() {
+        //given
+        Review review = new Review(reviewList, false);
+
+        //when
+        ReviewInput reviewInput = review.toReviewInput();
+
+        //then
+        assertThat(reviewInput.comments)
+                .hasSize(2)
+                .containsEntry("/src/main/java/file1.java", Collections.<ReviewFileComment>emptyList());
+        
     }
 
 }
