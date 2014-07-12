ae7847310aa5101b7c197fafab2ae27a8c923d61
Dominik Adam Przybysz <alien11689@gmail.com>
2014-07-12 17:05:25 +0200

    Add support for anylzing many files

--- src/test/java/pl/touk/sputnik/processor/codenarc/CodeNarcProcessorTest.java
+++ src/test/java/pl/touk/sputnik/processor/codenarc/CodeNarcProcessorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import pl.touk.sputnik.review.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,42 +12,45 @@ public class CodeNarcProcessorTest {
 
     CodeNarcProcessor sut = new CodeNarcProcessor();
 
+    String reviewFileWithOneViolationPath = "src/test/resources/codeNarcTestFiles/FileWithOneViolationLevel2.groovy";
+    String reviewFileWithViolationOnEachLevelPath = "src/test/resources/codeNarcTestFiles/FileWithOneViolationPerEachLevel.groovy";
+    String reviewFileWithoutViolationPath = "src/test/resources/codeNarcTestFiles/FileWithoutViolations.groovy";
+
     @Test
     public void shouldReturnSomeViolationsForFile() {
         //given
-        String reviewFilePath = "src/test/resources/codeNarcTestFiles/FileWithOneViolationLevel2.groovy";
-        ReviewFile reviewFile = new ReviewFile(reviewFilePath);
-        Review review = new Review(Arrays.asList(reviewFile));
+        Review review = getReview(reviewFileWithOneViolationPath);
         //when
         ReviewResult result = sut.process(review);
         //then
         assertThat(result).isNotNull();
-        assertThat(result.getViolations()).isNotNull().isNotEmpty().hasSize(1).contains(new Violation(reviewFilePath, 5, "EmptyTryBlock: The try block is empty", Severity.WARNING));
+        assertThat(result.getViolations())
+                .hasSize(1)
+                .contains(new Violation(reviewFileWithOneViolationPath, 5, "EmptyTryBlock: The try block is empty", Severity.WARNING));
     }
 
     @Test
     public void shouldReturnViolationsOfEachLevelForFile() {
         //given
-        String reviewFilePath = "src/test/resources/codeNarcTestFiles/FileWithOneViolationPerEachLevel.groovy";
-        ReviewFile reviewFile = new ReviewFile(reviewFilePath);
-        Review review = new Review(Arrays.asList(reviewFile));
+        Review review = getReview(reviewFileWithViolationOnEachLevelPath);
         //when
         ReviewResult result = sut.process(review);
         //then
         assertThat(result).isNotNull();
-        assertThat(result.getViolations()).isNotNull().isNotEmpty().hasSize(3).containsSequence(
-                new Violation(reviewFilePath, 5, "ForLoopShouldBeWhileLoop: The for loop can be simplified to a while loop", Severity.ERROR),
-                new Violation(reviewFilePath, 9, "AssertWithinFinallyBlock: A finally block within class FileWithOneViolationPerEachLevel contains an assert statement, potentially hiding the original exception, if there is one", Severity.WARNING),
-                new Violation(reviewFilePath, 13, "EmptyMethod: Violation in class FileWithOneViolationPerEachLevel. The method bar is both empty and not marked with @Override", Severity.INFO)
-        );
+        assertThat(result.getViolations())
+                .hasSize(3)
+                .containsSequence(
+                        new Violation(reviewFileWithViolationOnEachLevelPath, 5, "ForLoopShouldBeWhileLoop: The for loop can be simplified to a while loop", Severity.ERROR),
+                        new Violation(reviewFileWithViolationOnEachLevelPath, 9, "AssertWithinFinallyBlock: A finally block within class FileWithOneViolationPerEachLevel contains an assert statement, potentially hiding the original exception, if there is one", Severity.WARNING),
+                        new Violation(reviewFileWithViolationOnEachLevelPath, 13, "EmptyMethod: Violation in class FileWithOneViolationPerEachLevel. The method bar is both empty and not marked with @Override", Severity.INFO)
+                );
     }
 
     @Test
     public void shouldReturnNoViolationsForPerfectFile() {
         //given
-        String reviewFilePath = "src/test/resources/codeNarcTestFiles/FileWithoutViolations.groovy";
-        ReviewFile reviewFile = new ReviewFile(reviewFilePath);
-        Review review = new Review(Arrays.asList(reviewFile));
+        ReviewFile reviewFile = new ReviewFile(reviewFileWithoutViolationPath);
+        Review review = getReview(reviewFileWithoutViolationPath);
         //when
         ReviewResult result = sut.process(review);
         //then
@@ -58,11 +61,37 @@ public class CodeNarcProcessorTest {
     @Test
     public void shouldReturnNoViolationsWhenNoFiles() {
         //given
-        Review review = new Review(new ArrayList<ReviewFile>());
+        Review review = getReview();
         //when
         ReviewResult result = sut.process(review);
         //then
         assertThat(result).isNotNull();
         assertThat(result.getViolations()).isNotNull().isEmpty();
+    }
+
+    @Test
+    public void shouldReturnViolationsFromManyFiles() {
+        //given
+        Review review = getReview(reviewFileWithOneViolationPath, reviewFileWithoutViolationPath, reviewFileWithViolationOnEachLevelPath);
+        //when
+        ReviewResult result = sut.process(review);
+        //then
+        assertThat(result).isNotNull();
+        assertThat(result.getViolations())
+                .hasSize(4)
+                .containsSequence(
+                        new Violation(reviewFileWithOneViolationPath, 5, "EmptyTryBlock: The try block is empty", Severity.WARNING),
+                        new Violation(reviewFileWithViolationOnEachLevelPath, 5, "ForLoopShouldBeWhileLoop: The for loop can be simplified to a while loop", Severity.ERROR),
+                        new Violation(reviewFileWithViolationOnEachLevelPath, 9, "AssertWithinFinallyBlock: A finally block within class FileWithOneViolationPerEachLevel contains an assert statement, potentially hiding the original exception, if there is one", Severity.WARNING),
+                        new Violation(reviewFileWithViolationOnEachLevelPath, 13, "EmptyMethod: Violation in class FileWithOneViolationPerEachLevel. The method bar is both empty and not marked with @Override", Severity.INFO)
+                );
+    }
+
+    private Review getReview(String... filePaths) {
+        List<ReviewFile> files = new ArrayList<>();
+        for (String filePath : filePaths) {
+            files.add(new ReviewFile(filePath));
+        }
+        return new Review(files);
     }
 }
