27a1a370eb4ac6d2870ff85228cc4a38d1df8eca
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2014-07-31 21:37:37 +0200

    File extension filter

---
+++ src/main/java/pl/touk/sputnik/review/filter/FileExtensionFilter.java
@@ -0,0 +1,35 @@
+package pl.touk.sputnik.review.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import pl.touk.sputnik.review.ReviewFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Slf4j
+public class FileExtensionFilter {
+    private final List<ReviewFile> files;
+    private final List<String> allowedExtensions;
+
+    @NotNull
+    public List<ReviewFile> filter() {
+        log.info("Filtering out review files with allowed extensions {}", allowedExtensions);
+        List<ReviewFile> filtered = new ArrayList<>();
+
+        for (ReviewFile file : files) {
+            String extension = StringUtils.substringAfterLast(file.getReviewFilename(), ".");
+            if (allowedExtensions.contains(extension)) {
+                filtered.add(file);
+            } else {
+                log.info("File {} was filtered out due to not allowed extension {}", file.getReviewFilename(), extension);
+            }
+        }
+
+        log.info("Total {} of {} files had allowed extensions {}", filtered.size(), files.size(), allowedExtensions);
+        return filtered;
+    }
+}
