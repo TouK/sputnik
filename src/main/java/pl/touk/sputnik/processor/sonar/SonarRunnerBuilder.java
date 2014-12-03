8b35032c6faac4a22fc63d933af3ce2b13deb32b
Francois Visconte <f.visconte@criteo.com>
2014-12-03 18:23:31 +0100

    Use file basename instead of fullname in sonar
    Some sonar plugin do no include git path as the source file path (for
    example, sonar-visual-studio plugin generates filenames that are
    relative to the module csproj file).
    
    Using the file basename and a recursive match allows Sonar to match
    modified files with their indexed names in Sonar.
    
    Although this can generate useless analysis (as some files that are not
    included in a review may be analysed), this has a limited additional
    cost considering that few files have the same basename inside a
    repository.
    
    Change-Id: Ib74289e201098d15c618791f47b2dd657c3a1744

--- src/main/java/pl/touk/sputnik/processor/sonar/SonarRunnerBuilder.java
+++ src/main/java/pl/touk/sputnik/processor/sonar/SonarRunnerBuilder.java
@@ -1,5 +1,6 @@
 package pl.touk.sputnik.processor.sonar;
 
+import java.io.File;
 import java.util.List;
 
 import org.sonar.runner.api.EmbeddedRunner;
@@ -12,8 +13,22 @@ import com.google.common.collect.Lists;
 class SonarRunnerBuilder {
     public SonarRunner prepareRunner(Review review) {
         List<String> files = Lists.newArrayList();
+
+        /*
+         * Some sonar plugin do no include git path as the source file path (for
+         * example, sonar-visual-studio plugin generates filenames that are relative
+         * to the module csproj file).
+         *
+         * Using the file basename and a recursive match allows Sonar to match
+         * modified files with their indexed names in Sonar.
+         *
+         * Although this can generate useless analysis (as some files that are not
+         * included in a review may be analysed), this has a limited additional
+         * cost considering that few files have the same basename inside a
+         * repository.
+         */
         for (ReviewFile file : review.getFiles()) {
-            files.add(file.getReviewFilename());
+            files.add("**/" + new File(file.getReviewFilename()).getName());
         }
         SonarRunner sonarRunner = new SonarRunner(files, EmbeddedRunner.create());
         return sonarRunner;
