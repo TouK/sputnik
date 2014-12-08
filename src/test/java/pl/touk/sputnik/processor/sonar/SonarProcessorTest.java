ba8e29b611039614f755d0211f045e15d1552a12
Francois Visconte <f.visconte@criteo.com>
2014-12-08 10:15:38 +0100

    Use TestEnvironment class and fix naming
    Rename test and use TestEnvironment class as a base class for test
    in order to use helper methods.
    
    Change-Id: I413e2b5d536201dc59059b1fdc0a0c2c1073cc67

--- src/test/java/pl/touk/sputnik/processor/sonar/SonarProcessorTest.java
+++ src/test/java/pl/touk/sputnik/processor/sonar/SonarProcessorTest.java
@@ -10,16 +10,17 @@ import org.junit.Test;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.Resources;
 
+import pl.touk.sputnik.TestEnvironment;
 import pl.touk.sputnik.review.Review;
 import pl.touk.sputnik.review.ReviewFile;
 import pl.touk.sputnik.review.ReviewResult;
 import pl.touk.sputnik.review.Severity;
 import pl.touk.sputnik.review.Violation;
 
-public class SonarProcessorTest {
+public class SonarProcessorTest extends TestEnvironment {
 
     @Test
-    public void testFilterResults() {
+    public void shouldFilterResultFiles() {
         ReviewResult results = new ReviewResult();
         results.add(new Violation("src/t/f.cs", 0, "", Severity.ERROR));
         results.add(new Violation("src/t/f2.cs", 0, "", Severity.ERROR));
@@ -37,19 +38,17 @@ public class SonarProcessorTest {
     }
 
     @Test
-    public void testProcessor() {
+    public void shouldReportViolations() {
         SonarProcessor processor = new SonarProcessor(new SonarRunnerBuilder() {
             public SonarRunner prepareRunner(Review review) {
                 return new SonarRunner(null, null) {
                     public File run() throws IOException {
-                        return new File(Resources.getResource("json/sonar-result.json").getFile());
+                        return getResourceAsFile("json/sonar-result.json");
                     }
                 };
             }
         });
-        ReviewFile r1 = new ReviewFile("src/module2/dir/file2.cs");
-        Review review = new Review(ImmutableList.of(r1));
-        ReviewResult result = processor.process(review);
+        ReviewResult result = processor.process(nonexistantReview("src/module2/dir/file2.cs"));
         assertThat(result.getViolations()).hasSize(3);
     }
 }
