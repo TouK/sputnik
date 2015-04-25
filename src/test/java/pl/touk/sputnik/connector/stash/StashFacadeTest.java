adf72f97a158d5465797cf93496a3c0e8429486f
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2015-04-25 17:50:38 +0200

    Add comment once in Stash

--- src/test/java/pl/touk/sputnik/connector/stash/StashFacadeTest.java
+++ src/test/java/pl/touk/sputnik/connector/stash/StashFacadeTest.java
@@ -1,5 +1,16 @@
 package pl.touk.sputnik.connector.stash;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.github.tomakehurst.wiremock.client.UrlMatchingStrategy;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.google.common.collect.ImmutableList;
@@ -10,13 +21,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import pl.touk.sputnik.configuration.ConfigurationSetup;
 import pl.touk.sputnik.connector.FacadeConfigUtil;
-import pl.touk.sputnik.review.*;
+import pl.touk.sputnik.review.Review;
+import pl.touk.sputnik.review.ReviewFile;
+import pl.touk.sputnik.review.Severity;
+import pl.touk.sputnik.review.Violation;
 
 import java.util.List;
 import java.util.Map;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class StashFacadeTest {
 
@@ -62,9 +73,8 @@ public class StashFacadeTest {
 
         SingleFileChanges singleFileChanges = stashFacade.changesForSingleFile("src/main/java/Main.java");
         assertThat(singleFileChanges.getFilename()).isEqualTo("src/main/java/Main.java");
-        assertThat(singleFileChanges.getChangesMap())
-                .containsEntry(1, ChangeType.ADDED)
-                .containsEntry(2, ChangeType.ADDED);
+        assertThat(singleFileChanges.getChangeType(1)).isEqualTo(ChangeType.ADDED);
+        assertThat(singleFileChanges.getChangeType(2)).isEqualTo(ChangeType.ADDED);
     }
 
     @Test
