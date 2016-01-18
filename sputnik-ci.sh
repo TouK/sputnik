0462d0d4ae96fb8ce5f76e5289b9d14ae5eb4b3f
Rafał Nowak <rnw@touk.pl>
2016-01-18 17:18:39 +0100

    Correct version of properties download.

--- sputnik-ci.sh
+++ sputnik-ci.sh
@@ -9,6 +9,11 @@ fi
 if [ "$PR" != "false" ];
 then
   echo "Running on pull request $PR"
+  if [ ! -z "$api_key" ];
+  then
+    echo "Downloading sputnik.properties"
+    wget -q "http://sputnik.touk.pl/conf/$TRAVIS_REPO_SLUG/sputnik-properties?key=$api_key" -O sputnik.properties
+  fi
   wget "https://philanthropist.touk.pl/nexus/service/local/artifact/maven/redirect?r=snapshots&g=pl.touk&a=sputnik&c=all&v=LATEST" -O sputnik.jar && java -jar sputnik.jar --conf sputnik.properties --pullRequestId $PR
 fi
 
