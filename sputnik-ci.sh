86cff3fadd561c494b7df1debdc18a84563e2fc1
Rafał Nowak <rnw@touk.pl>
2016-01-18 16:01:14 +0100

    Sputnik SAAS script checks now PR Travis flag correctly, according to documentation.

--- sputnik-ci.sh
+++ sputnik-ci.sh
@@ -6,7 +6,7 @@ then
   PR="$TRAVIS_PULL_REQUEST"
 fi
 
-if [ ! -z "$PR" ];
+if [ "$PR" != "false" ];
 then
   echo "Running on pull request $PR"
   wget "https://philanthropist.touk.pl/nexus/service/local/artifact/maven/redirect?r=snapshots&g=pl.touk&a=sputnik&c=all&v=LATEST" -O sputnik.jar && java -jar sputnik.jar --conf sputnik.properties --pullRequestId $PR
