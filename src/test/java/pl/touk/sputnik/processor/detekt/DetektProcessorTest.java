6bc13fb2b63c808547eb4f3bfe9fa573aee329e6
Julien Lehuen <julien.lehuen@imc.com>
2020-10-18 20:30:40 +0200

    Bump Kotlin-related dependencies
    With this change, we bump all Kotlin-related dependencies to recent
    versions. This includes a Kotlin bump from 1.3 to 1.4, the migration
    from com.github.shyiko.ktlint to com.pinterest.ktlint and a significant
    upgrade of Detekt from 1.0.0-RC14 to 1.14.0.
    
    For running Detekt, a new tooling API was introduced. This means that
    the code that invoke Detekt can change not to use internal classes
    anymore. Calling a Kotlin DSL from Java looks a little verbose, but it
    still works nicely.
    
    For tests, the Detekt YAML file was reset from the latest default, then
    adapted slightly so the expected rules still get triggered. A few more
    minor changes were made to match expectations with the latest version.
    I also add a specific test for a new Detekt rule that has been added since
    the previous version we were using here...
    
    In terms of migration/changelong:
    
    - Changelog of Detekt RC versions:
      https://arturbosch.github.io/detekt/changelog-rc.html
    
    - Detekt include/exclude migration:
      https://arturbosch.github.io/detekt/howto-migratetestpattern.html
    
    - Release notes of Detekt, since:
      https://arturbosch.github.io/detekt/changelog.html
    
    - KtLint changelog:
      https://github.com/pinterest/ktlint/releases

--- src/test/java/pl/touk/sputnik/processor/detekt/DetektProcessorTest.java
+++ src/test/java/pl/touk/sputnik/processor/detekt/DetektProcessorTest.java
@@ -23,6 +23,7 @@ class DetektProcessorTest {
     private static final String VIOLATIONS_1 = "src/test/resources/detekt/testFiles/Violations1.kt";
     private static final String VIOLATIONS_2 = "src/test/resources/detekt/testFiles/sub/Violations2.kt";
     private static final String VIOLATIONS_3 = "src/test/resources/detekt/testFiles/Violations3.kt";
+    private static final String VIOLATIONS_4 = "src/test/resources/detekt/testFiles/Violations4.kt";
     private static final String REVIEW_GROOVY_FILE = "src/test/resources/codeNarc/testFiles/FileWithOneViolationLevel2.groovy";
 
     private DetektProcessor sut;
@@ -43,7 +44,7 @@ class DetektProcessorTest {
         assertThat(result).isNotNull();
         assertThat(result.getViolations())
                 .hasSize(3)
-                .contains(new Violation(VIOLATIONS_1, 14, "[style/NewLineAtEndOfFile] Checks whether files end with a line separator.", Severity.INFO))
+                .contains(new Violation(VIOLATIONS_1, 1, "[style/NewLineAtEndOfFile] Checks whether files end with a line separator.", Severity.INFO))
                 .contains(new Violation(VIOLATIONS_1, 3, "[style/WildcardImport] Wildcard imports should be replaced with imports using fully qualified class names. Wildcard imports can lead to naming conflicts. A library update can introduce naming clashes with your classes which results in compilation errors.", Severity.INFO))
                 .contains(new Violation(VIOLATIONS_1, 7, "[style/MagicNumber] Report magic numbers. Magic number is a numeric literal that is not defined as a constant and hence it's unclear what the purpose of this number is. It's better to declare such numbers as constants and give them a proper name. By default, -1, 0, 1, and 2 are not considered to be magic numbers.", Severity.INFO));
     }
@@ -58,8 +59,20 @@ class DetektProcessorTest {
         assertThat(result.getViolations())
                 .hasSize(3)
                 .contains(new Violation(VIOLATIONS_3, 3, "[empty-blocks/EmptyClassBlock] Empty block of code detected. As they serve no purpose they should be removed.", Severity.INFO))
-                .contains(new Violation(VIOLATIONS_2, 3, "[style/NewLineAtEndOfFile] Checks whether files end with a line separator.", Severity.INFO))
-                .contains(new Violation(VIOLATIONS_3, 4, "[style/NewLineAtEndOfFile] Checks whether files end with a line separator.", Severity.INFO));
+                .contains(new Violation(VIOLATIONS_2, 1, "[style/NewLineAtEndOfFile] Checks whether files end with a line separator.", Severity.INFO))
+                .contains(new Violation(VIOLATIONS_3, 1, "[style/NewLineAtEndOfFile] Checks whether files end with a line separator.", Severity.INFO));
+    }
+
+    @Test
+    void shouldReturnGlobalScopeViolation() {
+        Review review = getReview(VIOLATIONS_4);
+
+        ReviewResult result = sut.process(review);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getViolations())
+                .hasSize(1)
+                .contains(new Violation(VIOLATIONS_4, 7, "[coroutines/GlobalCoroutineUsage] Usage of GlobalScope instance is highly discouraged", Severity.ERROR));
     }
 
     @Test
